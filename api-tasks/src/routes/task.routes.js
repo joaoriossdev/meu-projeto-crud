679c94d07652814735900c8baeb4727443eab14f
joaoriossdev <joaobatistaprf1@gmail.com>
2026-05-14 18:41:43 -0300

    Adiciona configuração do VSCode, implementa rotas de tarefas e ajusta estrutura do servidor

--- api-tasks/src/routes/task.routes.js
+++ api-tasks/src/routes/task.routes.js
@@ -1,7 +1,14 @@
+//Nesta pasta ficam todas os endpoints possíveis que existem na minha API
+
+
+
+//Importando o módulo Router do Express para criar rotas e deixar o arquivo mais organizado
+//mantendo boas práticas de desenvolvimento.
+
 import { Router } from "express";
 const router = Router();
 
-
+//Essa rota é um exemplo para verificar se as rotas estão funcionando corretamente. Quando uma requisição GET é feita para a raiz da rota ("/"), ela responde com um JSON contendo uma mensagem de confirmação.
 router.get("/", (req, res) => {
     res.json({
         message: "Rota funcionando"
--- api-tasks/src/server.js
+++ api-tasks/src/server.js
@@ -1,4 +1,6 @@
+//Importações básicas 
 import express from "express";
+//Importação das rotas de tarefas para serem usadas no servidor
 import taskRoutes from "./routes/task.routes.js";
 const app = express();
 const PORT = 3000;
@@ -7,5 +9,5 @@ app.use(express.json());
 app.listen(3000, () => {
     console.log(`O servidor está rodando na porta ${PORT}`);
 });
-
+//Usando as rotas de tarefas para lidar com as requisições que começam com "/tasks"
 app.use("/tasks", taskRoutes);
